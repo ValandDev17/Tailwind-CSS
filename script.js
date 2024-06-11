940ac773df5e78730e72458a0ff61c2148d9c708
DevValand <155722873+Typroject2024@users.noreply.github.com>
2024-06-11 14:18:03 +0530

    Navbar Practice

---
+++ script.js
@@ -0,0 +1,5 @@
+const navDialog = document.getElementById("nav-dialog");
+
+function handleMenu(params) {
+    navDialog.classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors :{
+        "primary": "#3238f2",
+        "dev": "#01ee91"
+      },
+      fontFamily:{
+        "display": ['poppins', 'sans-serif'],
+        "body": ['inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
