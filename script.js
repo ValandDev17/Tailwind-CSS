730cc036fbcda05ed5accad1f8b61ceaf80f3c4d
ValandDev17 <valanddev17@gmail.com>
2024-06-12 23:05:16 +0530

    slider

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ const navDialog = document.getElementById("nav-dialog");
 function handleMenu(params) {
     navDialog.classList.toggle("hidden");
 }
+
+
+const initialTranLTR = -48 * 4;
+const initialTranRTL = 36 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        // console.log(element, isIntersecting);
+        if (isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+    intersectionObserver.observe(element);
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if (isLTR) {
+            totalTranslate = translateX + initialTranLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranRTL);
+
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
